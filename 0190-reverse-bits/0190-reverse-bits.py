f385e70976a54795a8bb00f25dc017e3fca172f6
Cesar Emilio Castaño Marin <65086997+CesarEmilioC@users.noreply.github.com>
2023-10-15 01:21:02 -0700

    Time: 16 ms (54.12%), Space: 13.3 MB (11.43%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,11 +1,20 @@
+'''
+Cesar Emilio Castaño Marin
+Solution: Reverse Bits
+Github username: CesarEmilioC
+'''
 class Solution:
     # @param n, an integer
     # @return an integer
     def reverseBits(self, n):
+        #We define the new number that is going to be calculated
         num = 0
         for i in range(31,-1,-1):
+            #Every power of 2 that is less than the current number, represents a 1 in the binary form
             if 2 ** i <= n:
+                #We sum up the new power of two (like if the number was reversed)
                 num += 2 ** (31 - i)
+                #We update the number
                 n -= 2 ** i
         return num
         
