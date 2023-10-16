6e756942250de802ac0406ea027ff5aaee846560
Cesar Emilio Castaño Marin <65086997+CesarEmilioC@users.noreply.github.com>
2023-10-15 20:20:29 -0700

    Time: 364 ms (18.65%), Space: 14.1 MB (34.83%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -0,0 +1,31 @@
+'''
+Cesar Emilio Castaño Marin
+Solution: Longest Substring Without Repeating Characters
+Github Username: CesarEmilioC
+'''
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #Special cases
+        if s == "":
+            return 0
+        elif len(s) == 1:
+            return 1
+        #We create a max lenght variable that is going to be updated in the loop
+        max_lenght = 0
+        for i in range(len(s)):
+            substring = s[i]
+            contador = 0
+            #We check the substring with the largest lenght and without repeated symbols
+            for letra in s[i+1:]:
+                if letra not in substring:
+                    substring += letra
+                    contador += 1
+                else:
+                    break
+            if max_lenght < len(substring):
+                max_lenght = len(substring)
+        return max_lenght
